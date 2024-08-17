2d1f5984dd36166aa897acd795186c0008a23641
Serhii <serhii.chabanow.work@gmail.com>
2024-08-17 11:32:04 +0300

    test

--- README.md
+++
@@ -1,1 +0,0 @@
-Here will be my own site!)
---
+++ gatsby-starter-hello-world/src/pages/index.js
@@ -0,0 +1,16 @@
+import * as React from "react"
+
+export default function Home() {
+  return (
+    <main>
+      <h1>serhiichabanow.com</h1>
+    </main>
+  )
+}
+
+
+
+
+// --font-family-monospace: "IBM Plex Mono", monospace;
+// --font-family-heading: "Plus Jakarta Sans", -apple-system, BlinkMacSystemFont, "Helvetica Neue", Arial, sans-serif;
+// --font-family-nav: "Inter", -apple-system, BlinkMacSystemFont, "Helvetica Neue", Arial, sans-serif;
